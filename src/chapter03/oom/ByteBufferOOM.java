4cb701c10d28d6dd95c6287a9222816c4ba70e6d
Stern <kossqwas@gmail.com>
2021-01-27 20:57:25 +0800

    Add tools.jar

--- src/chapter03/oom/ByteBufferOOM.java
+++ src/chapter03/oom/ByteBufferOOM.java
@@ -1,14 +1,17 @@
 package chapter03.oom;
 
+
+import sun.nio.ch.DirectBuffer;
+
 import java.nio.ByteBuffer;
 
 public class ByteBufferOOM {
 
 	//-XX:MaxDirectMemorySize=256m
 	public static void main(String []args) {
-		ByteBuffer.allocateDirect(257 * 1024 * 1024);
-		//DirectBuffer byteBuffer = (DirectBuffer)ByteBuffer.allocateDirect(256 * 1024 * 1024);
-		//((DirectBuffer)byteBuffer).cleaner().clean();
-		//ByteBuffer.allocateDirect(1024 * 1024);
+		//ByteBuffer.allocateDirect(257 * 1024 * 1024);
+		Object o = ByteBuffer.allocateDirect(256 * 1024 * 1024);
+	//	((DirectBuffer) o).cleaner().clean();
+		ByteBuffer.allocateDirect(1024 * 1024);
 	}
 }
